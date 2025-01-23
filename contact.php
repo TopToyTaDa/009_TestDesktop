3091339b9fbcd08a575372e3da9df07dea33b812
TopToyTaDa <top0814765001@gmail.com>
2025-01-23 11:10:43 +0700

    แก้ไขไฟล์ครั้งที่ 1
    แก้ไขไฟล์ครั้งที่ 1

--- contact.php
+++ contact.php
@@ -17,6 +17,7 @@
 				<li><a href="room.php">ห้องพัก&ราคา</li>
 				<li><a herf="guest.php">สมุดเยี่ยม</li>
 				<li><a href="contact.php">ติดต่อ</li>
+                <li><a href="contact.php">ท่านกำลังเข้าสู่บริการติดต่อ</li>
 				</ul><p>
 				<?php include "login_form.php";?>
 			</td>
